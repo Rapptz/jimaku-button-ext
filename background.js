22be10b890befa94fabb6295dc9ddd544d40e0e9
Rapptz <rapptz@gmail.com>
2024-07-15 02:06:11 -0400

    Initial commit

---
+++ background.js
@@ -0,0 +1,161 @@
+const anilistRegex = /^https:\/\/anilist\.co\/anime\/(\d+)\//m;
+const anilistUserRegex = /^https:\/\/anilist.co\/user\/(.+)\//m;
+const tmdbRegex = /^https:\/\/(?:www\.)?themoviedb\.org\/(tv|movie)\/(\d+)(?:-[a-zA-Z0-9\-]+)?(?:\/.*)?/m;
+const COOLDOWN_DELAY = 10 * 60 * 1000;
+let apiKey = null;
+const shortTermCache = new Map();
+
+const getAnilistId = (url) => {
+  const m = url.match(anilistRegex);
+  return m == null || m.length !== 2 ? null : parseInt(m[1], 10);
+}
+
+const getAnilistUsername = (url) => {
+  const m = url.match(anilistUserRegex);
+  return m == null || m.length !== 2 ? null : m[1];
+}
+
+const getTmdbId = (url) => {
+  const m = url.match(tmdbRegex);
+  return m == null || m.length !== 3 ? null : { type: m[1], id: parseInt(m[2], 10) };
+}
+
+async function clearCredentials() {
+  apiKey = null;
+  await chrome.storage.local.remove('apiKey');
+}
+
+async function removeCacheCooldown(cacheKey) {
+  let cache = await chrome.storage.local.get(cacheKey);
+  if(cache.hasOwnProperty(cacheKey)) {
+    let info = cache[cacheKey];
+    if(info.cooldown) {
+      await chrome.storage.local.remove(cacheKey);
+    }
+  }
+}
+
+async function setCacheCooldown(cacheKey) {
+  await chrome.storage.local.set({
+    [cacheKey]: {
+      cooldown: true,
+    }
+  });
+  setTimeout(() => removeCacheCooldown(cacheKey), COOLDOWN_DELAY);
+}
+
+async function searchService(queryName, cacheKey) {
+  console.log(cacheKey);
+  let cache = await chrome.storage.local.get(cacheKey);
+  if(cache.hasOwnProperty(cacheKey)) {
+    let info = cache[cacheKey];
+    if(info.cooldown) {
+      return null;
+    }
+    return `https://jimaku.cc/entry/${info.id}`;
+  }
+
+  let response = await fetch(`https://jimaku.cc/api/entries/search?${queryName}=${cacheKey}`, {
+    headers: {
+      'authorization': apiKey,
+      'user-agent': 'Rapptz/jimaku-button-ext',
+    }
+  });
+  if(response.status !== 200) {
+    if(response.status === 401) {
+      await clearCredentials();
+    }
+    setCacheCooldown(cacheKey);
+    return null;
+  }
+  let data = await response.json();
+  if(data.length === 0) {
+    setCacheCooldown(cacheKey);
+    return null;
+  }
+  let entryId = data[0].id;
+  await chrome.storage.local.set({
+    [cacheKey]: {
+      id: entryId
+    }
+  });
+  return `https://jimaku.cc/entry/${entryId}`;
+}
+
+async function handleTabAction(tab) {
+  if(apiKey === null) {
+    await chrome.pageAction.show(tab.id);
+    return;
+  }
+
+  let anilistId = getAnilistId(tab.url);
+  if(anilistId !== null) {
+    let url = await searchService('anilist_id', anilistId.toString());
+    if(url !== null) {
+      shortTermCache.set(tab.id, url);
+      await chrome.pageAction.show(tab.id);
+      return;
+    }
+  } else {
+    let anilistUsername = getAnilistUsername(tab.url);
+    if(anilistUsername !== null) {
+      let url = `https://jimaku.cc/anilist/${anilistUsername}`;
+      shortTermCache.set(tab.id, url);
+      await chrome.pageAction.show(tab.id);
+      return;
+    }
+    let tmdbId = getTmdbId(tab.url);
+    if(tmdbId !== null) {
+      let cacheKey = `${tmdbId.type}:${tmdbId.id}`;
+      let url = await searchService('tmdb_id', cacheKey);
+      if(url !== null) {
+        shortTermCache.set(tab.id, url);
+        await chrome.pageAction.show(tab.id);
+        return;
+      }
+    }
+  }
+
+  await chrome.pageAction.hide(tab.id);
+}
+
+async function main() {
+  let activeTab = await chrome.tabs.query({active: true, currentWindow: true});
+  await handleTabAction(activeTab[0]);
+  let storedApiKey = await chrome.storage.local.get('apiKey');
+  apiKey = storedApiKey.apiKey ?? null;
+
+  chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if(!changeInfo.url) {
+      return;
+    }
+    let getTab = await chrome.tabs.query({active: true, currentWindow: true});
+    let activeTab = getTab[0];
+    if(tabId === activeTab.id) {
+      await handleTabAction(activeTab);
+    }
+  });
+
+  chrome.pageAction.onClicked.addListener((tab) => {
+    if(apiKey === null) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      let url = shortTermCache.get(tab.id);
+      if(url !== null) {
+        chrome.tabs.create({url});
+      }
+    }
+  });
+
+  chrome.storage.onChanged.addListener((changes, area) => {
+    if(area !== "local") {
+      return;
+    }
+    let changedApiKey = changes['apiKey'];
+    if(changedApiKey != null) {
+      apiKey = changedApiKey?.newValue ?? null;
+    }
+  })
+}
+
+main();
---
+++ options.js
@@ -0,0 +1,26 @@
+const apiKey = document.getElementById('api-key');
+async function saveOptions(e) {
+  e.preventDefault();
+  const testRequest = await fetch('https://jimaku.cc/api/entries/729', {
+    headers: {
+      'authorization': apiKey.value,
+      'user-agent': 'Rapptz/jimaku-button-ext',
+    }
+  });
+  console.log(testRequest.status, apiKey.value);
+  if(testRequest.status === 401) {
+    apiKey.setCustomValidity('Improper API key.');
+    return;
+  }
+  await chrome.storage.local.set({
+    apiKey: apiKey.value
+  });
+}
+
+async function restoreOptions() {
+  let res = await chrome.storage.local.get('apiKey');
+  apiKey.value = res.apiKey ?? '';
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('form').addEventListener('submit', saveOptions);
